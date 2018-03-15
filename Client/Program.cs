38f4e5c766e2d31e5b8a76323d9f4ad5248659f1
David Azevedo <davidazevedo1996@hotmail.com>
2018-03-15 19:36:24 +0000

    Added client files

---
+++ Client/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.Remoting;
+using TDIN_Proj1;
+
+namespace Client
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Form1 f = new Form1();
+            Application.Run(f);
+
+            f.setLabel("Boas");
+
+            // RemotingConfiguration.Configure("Client.exe.config", false);
+
+            IDiginoteSystem system = (IDiginoteSystem) RemotingServices.Connect(typeof(IDiginoteSystem),
+                "tpc://192.168.137.223:999/server/diginoteEndpoint");
+            
+            f.setLabel(system.ReturnHello());
+
+
+        }
+    }
+}
