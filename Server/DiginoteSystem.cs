f52ce4c4b8232a327507ea197c83d15788c3bcfe
David Azevedo <davidazevedo1996@hotmail.com>
2018-03-15 18:25:37 +0000

    Added some class files

---
+++ Server/DiginoteSystem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server
+{
+    class DiginoteSystem
+    {
+        private static double QUOTE = 1.00;
+
+        public double GetCurrentQuote() {
+            // TODO if logged in
+            return QUOTE;
+        }
+
+        public void Register(string name, string nickname, string password) { }
+
+        public void Logout(string nickname, string password) { }
+
+        public Boolean Login(string nickname, string password) {
+            return false;
+        }
+
+    }
+}
---
+++ Server/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server.Models
+{
+    class User
+    {
+        private string name;
+        private string nickname;
+        private string password;
+    }
+}
